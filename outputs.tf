57214c73f7eaebc5056a11126469c58f44b8e4a5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "dev_test_policies" {
-  description = "All dev_test_policy resources"
-  value       = azurerm_dev_test_policy.dev_test_policies
-}
 output "dev_test_policies_description" {
-  description = "List of description values across all dev_test_policies"
-  value       = [for k, v in azurerm_dev_test_policy.dev_test_policies : v.description]
+  description = "Map of description values across all dev_test_policies, keyed the same as var.dev_test_policies"
+  value       = { for k, v in azurerm_dev_test_policy.dev_test_policies : k => v.description }
 }
 output "dev_test_policies_evaluator_type" {
-  description = "List of evaluator_type values across all dev_test_policies"
-  value       = [for k, v in azurerm_dev_test_policy.dev_test_policies : v.evaluator_type]
+  description = "Map of evaluator_type values across all dev_test_policies, keyed the same as var.dev_test_policies"
+  value       = { for k, v in azurerm_dev_test_policy.dev_test_policies : k => v.evaluator_type }
 }
 output "dev_test_policies_fact_data" {
-  description = "List of fact_data values across all dev_test_policies"
-  value       = [for k, v in azurerm_dev_test_policy.dev_test_policies : v.fact_data]
+  description = "Map of fact_data values across all dev_test_policies, keyed the same as var.dev_test_policies"
+  value       = { for k, v in azurerm_dev_test_policy.dev_test_policies : k => v.fact_data }
 }
 output "dev_test_policies_lab_name" {
-  description = "List of lab_name values across all dev_test_policies"
-  value       = [for k, v in azurerm_dev_test_policy.dev_test_policies : v.lab_name]
+  description = "Map of lab_name values across all dev_test_policies, keyed the same as var.dev_test_policies"
+  value       = { for k, v in azurerm_dev_test_policy.dev_test_policies : k => v.lab_name }
 }
 output "dev_test_policies_name" {
-  description = "List of name values across all dev_test_policies"
-  value       = [for k, v in azurerm_dev_test_policy.dev_test_policies : v.name]
+  description = "Map of name values across all dev_test_policies, keyed the same as var.dev_test_policies"
+  value       = { for k, v in azurerm_dev_test_policy.dev_test_policies : k => v.name }
 }
 output "dev_test_policies_policy_set_name" {
-  description = "List of policy_set_name values across all dev_test_policies"
-  value       = [for k, v in azurerm_dev_test_policy.dev_test_policies : v.policy_set_name]
+  description = "Map of policy_set_name values across all dev_test_policies, keyed the same as var.dev_test_policies"
+  value       = { for k, v in azurerm_dev_test_policy.dev_test_policies : k => v.policy_set_name }
 }
 output "dev_test_policies_resource_group_name" {
-  description = "List of resource_group_name values across all dev_test_policies"
-  value       = [for k, v in azurerm_dev_test_policy.dev_test_policies : v.resource_group_name]
+  description = "Map of resource_group_name values across all dev_test_policies, keyed the same as var.dev_test_policies"
+  value       = { for k, v in azurerm_dev_test_policy.dev_test_policies : k => v.resource_group_name }
 }
 output "dev_test_policies_tags" {
-  description = "List of tags values across all dev_test_policies"
-  value       = [for k, v in azurerm_dev_test_policy.dev_test_policies : v.tags]
+  description = "Map of tags values across all dev_test_policies, keyed the same as var.dev_test_policies"
+  value       = { for k, v in azurerm_dev_test_policy.dev_test_policies : k => v.tags }
 }
 output "dev_test_policies_threshold" {
-  description = "List of threshold values across all dev_test_policies"
-  value       = [for k, v in azurerm_dev_test_policy.dev_test_policies : v.threshold]
+  description = "Map of threshold values across all dev_test_policies, keyed the same as var.dev_test_policies"
+  value       = { for k, v in azurerm_dev_test_policy.dev_test_policies : k => v.threshold }
 }
 
